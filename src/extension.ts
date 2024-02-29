219d09d2ba1fef42e2bfdf7ad0ed897670ddf344
yangml103 <anthony.yang103@gmail.com>
2024-02-29 12:23:13 -0500

    Removed some comments

--- src/extension.ts
+++ src/extension.ts
@@ -8,18 +8,11 @@ let totalLinesOfCode = 0;
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
+
 	console.log('Congratulations, your extension "lifetime-lines-of-code" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	//let disposable = vscode.commands.registerCommand('lifetime-lines-of-code.helloWorld', () => {
 	let disposable = vscode.commands.registerCommand('lifetime-lines-of-code.countLines', () => {
-	
-	
-		
+			
 		// Access active workspace
 		const currFolders = vscode.workspace.workspaceFolders;
 
