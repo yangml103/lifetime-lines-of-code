9604501eea0395d8cf2de11a1ba1f900c1078e7e
Anthony Yang <65149795+yangml103@users.noreply.github.com>
2024-02-29 13:01:31 -0500

    Create folder selector (#1)
    * Removed some comments
    
    * Added file selector option, and changed vscodeignore to ignore test folders and include .ts files
    
    * Updated README with updated languages - C, SQL etc, and also started the CHANGELOG.
    
    * Cleaned up comments

--- src/extension.ts
+++ src/extension.ts
@@ -8,41 +8,45 @@ let totalLinesOfCode = 0;
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
+	let languages = ['Python', 'Java', 'JavaScript', 'TypeScript', 'HTML', 'CSS', 'SQL', 'C#', 'C++', 'C'];
+	let extensions = ['py', 'java', 'js', 'ts', 'html', 'css', 'sql', 'cs', 'cpp', 'c'];
+
+
 	console.log('Congratulations, your extension "lifetime-lines-of-code" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	//let disposable = vscode.commands.registerCommand('lifetime-lines-of-code.helloWorld', () => {
 	let disposable = vscode.commands.registerCommand('lifetime-lines-of-code.countLines', () => {
-	
-	
-		
-		// Access active workspace
-		const currFolders = vscode.workspace.workspaceFolders;
-
-		if (!currFolders){
-			vscode.window.showErrorMessage('No workspace found.');
-			return;
-		}
-
-		// Iterate over workspace folders
-		for(const workspaceFolder of currFolders){
 			
 
+		// let user select which folder to iterate through 
+		const folders = vscode.window.showOpenDialog({
+			canSelectFiles: true,
+			canSelectFolders: true,
+			canSelectMany: true,
+		});
+
+		
+		
+		folders.then(folders => { 
+			// Iterate over workspace folders
+			if (!folders){
+				vscode.window.showErrorMessage('No folder selected.');
+				return;
+			}
+
+			for(const workspaceFolder of folders){
+				
 			// Search for files in the workspace folder
-			vscode.workspace.findFiles(new vscode.RelativePattern(workspaceFolder,'**/*.*'),'',5000) // matches all files, up to 5000
+			vscode.workspace.findFiles(new vscode.RelativePattern(workspaceFolder,'**/*.*'),'') // matches all files
 			.then((files:vscode.Uri[]) =>{
 				for(const file of files){
 					// Process each file
 					countLinesOfCode(file.fsPath);
 				}
-				console.log(`Total lines of code: ${totalLinesOfCode}`);
+				console.log(`Total lines of (Python, Java, JavaScript, TypeScript, HTML, CSS, SQL, C#, C++, C) code: ${totalLinesOfCode}`);
 			});
 
 		}
+	});
 
 	});
 
@@ -52,8 +56,8 @@ export function activate(context: vscode.ExtensionContext) {
 function countLinesOfCode(filePath:string){
 
 	const fileExtension:string = filePath.split('.').pop() || '';
-// Check if the file is a Python, JavaScript, Java, TypeScript, HTML, or CSS file
-	if (['py', 'js', 'java', 'ts', 'html', 'css'].includes(fileExtension)) {
+// Check if the file is a Python, JavaScript, Java, TypeScript, HTML, CSS, SQL, C#, C++ or C file
+	if (['py', 'js', 'java', 'ts', 'html', 'css','sql','cs','cpp','c'].includes(fileExtension)) {
 		const fileContent = fs.readFileSync(filePath, 'utf-8');
 		const lines = fileContent.split('\n').filter(line => line.trim() !== '');
 
