e52c3ded14d553622ca5632565fb6802bf8141aa
Anthony Yang <65149795+yangml103@users.noreply.github.com>
2024-03-06 13:39:21 -0500

    Test1 (#8)

--- src/extension.ts
+++ src/extension.ts
@@ -98,5 +98,6 @@ export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(disposable);
 }
 
+
 // This method is called when your extension is deactivated
 export function deactivate() {}
